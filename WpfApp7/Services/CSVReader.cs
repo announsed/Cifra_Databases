c44b17064e9cc93d1c4bdc97edac9084f14b6d43
announsed <147243328+announsed@users.noreply.github.com>
2025-11-06 22:42:39 +0300

    недоделки 06.11.25

---
+++ WpfApp7/Services/CSVReader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper;
+using WpfApp7.Models;
+
+namespace WpfApp7.Services
+{
+    internal class CSVReader
+    {
+        public List<TitanicPassengers> Passengers { get; set; }
+
+        public CSVReader()
+        {
+            using (StreamReader reader = new StreamReader("titanic.csv"))
+            {
+                CsvReader csvReader = new CsvReader(reader, CultureInfo.InvariantCulture);
+
+                Passengers = csvReader.GetRecords<TitanicPassengers>().ToList();
+
+            }
+        }
+    }
+}
