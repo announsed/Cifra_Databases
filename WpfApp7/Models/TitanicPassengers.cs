32f50280fe75b995e9344a39b89ffbf880216311
announsed <147243328+announsed@users.noreply.github.com>
2025-11-08 20:57:44 +0300

    Урок 95. Практика WPF 08.11.25

--- WpfApp7/Models/TitanicPassengers.cs
+++ WpfApp7/Models/TitanicPassengers.cs
@@ -4,10 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WpfApp7.Data
+namespace WpfApp7.Models
 {
     internal class TitanicPassengers
     {
-
+        public int PassengerID { get; set; }
+        public string Name { get; set; }
+        public string PClass { get; set; }
+        public string Age { get; set; }
+        public string Sex { get; set; }
+        public string Survived { get; set; }
+        public string SexCode { get; set; }
     }
 }
--- WpfApp7/ViewModels/PassengersViewModel.cs
+++ WpfApp7/ViewModels/PassengersViewModel.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WpfApp7.Models;
 
 namespace WpfApp7.ViewModels
 {
-    internal class PassengersViewModel
+    internal class PassengersViewModel : ViewModelBase
     {
+        private TitanicPassengers _selectedPassengers;
+
     }
 }
