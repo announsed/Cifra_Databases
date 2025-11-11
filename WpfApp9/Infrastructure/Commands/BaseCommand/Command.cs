a60f1cb50920bcf2f24216cae1a3b893de43463d
announsed <147243328+announsed@users.noreply.github.com>
2025-11-11 22:36:41 +0300

    Cifra 11.11.2025

---
+++ WpfApp9/Infrastructure/Commands/BaseCommand/Command.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApp9.Infrastructure.Commands.BaseCommand
+{
+    internal abstract class Command : ICommand
+    {
+        public event EventHandler CanExecuteChanged 
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public abstract bool CanExecute(object? command);
+        public abstract void Execute(object? command);
+    }
+}
---
+++ WpfApp9/Services/GradientGenerate.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace WpfApp9.Services
+{
+    internal static class GradientGenerate
+    {
+        private static LinearGradientBrush? _linerGradientBrush;
+        private static GradientStopCollection? _gradientStopCollectionRed;
+        private static GradientStopCollection GetRedGradientCollection() 
+        {
+            GradientStopCollection redGradientStopCollection = new();
+            redGradientStopCollection.Add(new GradientStop(Colors.Black, 0.0));
+            redGradientStopCollection.Add(new GradientStop(Colors.Red, 0.25));
+            redGradientStopCollection.Add(new GradientStop(Colors.Black, 0.5));
+            redGradientStopCollection.Add(new GradientStop(Colors.Red, 0.75));
+            redGradientStopCollection.Add(new GradientStop(Colors.Black, 1.0));
+            return redGradientStopCollection;
+        }
+        private static GradientStopCollection GetGreenGradientCollection() 
+        {
+            GradientStopCollection greenGradientStopCollection = new();
+            greenGradientStopCollection.Add(new GradientStop(Colors.Black, 0.0));
+            greenGradientStopCollection.Add(new GradientStop(Colors.Red, 0.25));
+            greenGradientStopCollection.Add(new GradientStop(Colors.Black, 0.5));
+            greenGradientStopCollection.Add(new GradientStop(Colors.Red, 0.75));
+            greenGradientStopCollection.Add(new GradientStop(Colors.Black, 1.0));
+            return greenGradientStopCollection;
+        }
+        public static LinearGradientBrush GetRedGradientBrush() 
+        {
+            _linerGradientBrush = new LinearGradientBrush(GetRedGradientCollection());
+            return _linerGradientBrush;
+        }
+
+        public static LinearGradientBrush GetGreenGradientBrush()
+        {
+            _linerGradientBrush = new LinearGradientBrush(GetGreenGradientCollection());
+            return _linerGradientBrush;
+        }
+    }
+}
