0421e0d3f5018581dd74e751244e05f757b45e47
announsed <147243328+announsed@users.noreply.github.com>
2025-12-23 22:03:54 +0300

    Cifra 23.12.25 Логирование

---
+++ WebApplicationWether/IRandomService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Runtime.InteropServices.Marshalling;
+
+namespace WebApplicationWether
+{
+    public interface IRandomService<T>
+    {
+        public T GetRandom<T>();
+    }
+
+}
--- WebApplicationWether/Program.cs
+++ WebApplicationWether/Program.cs
@@ -1,5 +1,6 @@
 
 using System.Net;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.EntityFrameworkCore;
 
 namespace WebApplicationWether
@@ -9,6 +10,14 @@ namespace WebApplicationWether
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+
+            // Добавление логирования в приложение
+            builder.Logging.ClearProviders(); // Очищает стандартные провайдеры
+            builder.Logging.AddConsole(); // Добавляет консольный провайдер
+            builder.Logging.AddDebug(); // Добавляет провайдер для Visual Studio Debug
+
+
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
             {
                 options.UseSqlite("DataSource=myDatabase.db");
@@ -32,6 +41,57 @@ namespace WebApplicationWether
             {
                 app.MapOpenApi();
             }
+
+            app.UseOpenApi();
+            app.UseSwaggerUi(c =>
+            {
+                c.DocExpansion = "c";
+            });
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+
+
+
+            app.UseMiddleware<CastomMWcs>();
+            app.UseMiddleware<CastomMW2cs>();
+
+
+            app.MapControllers();
+
+
+            app.Use(async (context, next) =>
+            {
+                Console.WriteLine("Второй Middleware: Обработка запроса");
+                await next();
+            });
+            app.Run(async context =>
+            {
+                Console.WriteLine("Третий Middleware: Финальный обработчик");
+                await context.Response.WriteAsync("Hello from ASP.NET Core!");
+            });
+
+
+            //Обработка ошибок и логирование
+            app.UseExceptionHandler(errorApp =>
+            {
+                errorApp.Run(async context =>
+                {
+                    var logger =
+                    context.RequestServices.GetRequiredService<ILogger<Program>>();
+                    var exception =
+                    context.Features.Get<IExceptionHandlerFeature>()?.Error;
+                    
+                    logger.LogError(exception, "Произошла ошибка: {Message}",
+                    exception?.Message);
+                    await context.Response.WriteAsync("Произошла ошибка.");
+                });
+            });
+
+
+
+            // Регистрация сервисов
+
             app.MapGet("/library/{authorId}/{bookId}", async (int authorId, int bookId, HttpContext context) =>
             {
                 var error = context.Response.StatusCode = StatusCodes.Status400BadRequest;
@@ -56,40 +116,6 @@ namespace WebApplicationWether
 
             });
 
-            app.UseOpenApi();
-            app.UseSwaggerUi(c =>
-            {
-                c.DocExpansion = "c";
-            });
-
-            app.UseHttpsRedirection();
-            app.UseAuthorization();
-
-
-
-            //app.UseMiddleware<CastomMWcs>();
-            //app.UseMiddleware<CastomMW2cs>();
-
-
-            app.MapControllers();
-
-
-            //app.Use(async (context, next) =>
-            //{
-            //    Console.WriteLine("Второй Middleware: Обработка запроса");
-            //    await next();
-            //});
-            //app.Run(async context =>
-            //{
-            //    Console.WriteLine("Третий Middleware: Финальный обработчик");
-            //    await context.Response.WriteAsync("Hello from ASP.NET Core!");
-            //});
-
-
-
-
-            // Регистрация сервисов
-
             app.MapGet("/products", async (IProductService productService) =>
             {
                 var products = productService.GetAllProducts();
@@ -113,8 +139,45 @@ namespace WebApplicationWether
             {
                 var product = new Product() {Name = name, Price = prise };
                 productService.AddProduct(product, out string? ffalse);
+                if(ffalse != null)
+                    Console.WriteLine(ffalse);
 
                 return product;
+            });
+
+
+            // Логирование
+            app.MapGet("/", (ILogger<Program> logger) =>
+            {
+                logger.LogInformation("Получен запрос на главную страницу.");
+                return Results.Ok("Welcome to the home page!");
+            });
+
+
+            //Уровни логирования
+            app.MapGet("/log-levels", (ILogger<Program> logger) =>
+            {
+                logger.LogTrace("Это Trace лог.");
+                logger.LogDebug("Это Debug лог.");
+                logger.LogInformation("Это Information лог.");
+                logger.LogWarning("Это Warning лог.");
+                logger.LogError("Это Error лог.");
+                logger.LogCritical("Это Critical лог.");
+                return Results.Ok("Логи записаны.");
+            });
+
+
+            // Практика логирования
+            app.MapGet("/user/{id}", async(IProductService product, int id) => 
+            {
+                var allProduct = product.GetAllProducts();
+                var allId = allProduct.Where(p => p.Id == id);
+                var res = Results.Ok(product.GetProductById(id));
+                if (allId == null) 
+                {
+                    res = (IResult)Results.Extensions;
+                }
+                 return res;
             });
 
             app.Run();
